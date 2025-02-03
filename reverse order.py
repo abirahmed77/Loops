b978646407ec1b337cdd3c0857601607061011a2
Abir <abirgpx@gmail.com>
2025-02-03 13:33:35 +0600

    first commit

---
+++ reverse order.py
@@ -0,0 +1,4 @@
+print("Reversed order from 10 to 0 : ")
+
+for i in range(10, -1, -1):
+    print(i)
---
+++ sum of natural number.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a Number : "))
+
+sum = 0
+
+for i in range(0, n):
+    sum = sum + i
+    print(f"sum if {i} iteration : {sum}")
